5c8a720b8db66f13c74ed6afdf583b910a455f53
Abolfazl Ghorbani <ab.ghorbani28@gmail.com>
2022-07-01 10:05:04 +0200

    Create-Basic-Example-Send message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import emailjs from 'emailjs-com';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
+
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_v7x1yvk', 'template_bqsducs', e.target,'user_afhNIbAa8jkksGjPo6RiK')
+    .then(result => {
+      if(result.status === 200){
+        toast.success("Your message has been successfully sent",{position: "top-right", closeOnClick: true})
+      }
+    },(error) =>{
+      console.log(error)
+    })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container py-5">
+        <div className="row py-5 justify-content-center">
+          <div className="col-md-4 bg-white p-5 mt-5">
+            <h2 className="pb-5 text-center">Send message to management</h2>
+            <form onSubmit={sendEmail}>
+              <div className="form-group">
+                <input type="text" name="name" className="form-control p-3" placeholder="your name" />
+              </div>
+              <div className="form-group">
+                <input type="text" name="email" className="form-control p-3" placeholder="Your Email" />
+              </div>
+              <div className="form-group">
+                <textarea name="message" className="form-control p-3" placeholder="Your message"></textarea>
+              </div>
+              <div className="form-group">
+                <input type="submit" className="btn btn-success w-100 p-2 text-white mt-5" value="submit
+" />
+              </div>
+            </form>
+          </div>
+        </div>
+        <ToastContainer />
     </div>
   );
 }
